3bfab0f7e05806f066cf9ef50ad5b45a6c71bd30
Firdavs-22 <firdavsgaybullayev22@gmail.com>
2025-04-08 13:07:38 +0900

    added in username the case sensitive

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterRequest extends FormRequest
@@ -23,11 +24,24 @@ class RegisterRequest extends FormRequest
     {
         return [
             "name" => ["required", "string", "min:1", "max:255"],
-            "username" => ["required", "string", "min:1", "max:255", "unique:users,username"],
+            "username" => ["required", "string", "min:3", "max:15", "regex:/^\S*$/U",
+                function ($attribute, $value, $fail) {
+                    $exists = User::whereRaw("username like ?",[$value])->exists();
+                    if ($exists) {
+                        $fail("The username has already been taken.");
+                    }
+                }],
             "email" => ["required", "string", "email", "max:255", "unique:users,email"],
             "password" => ["required", "string", "min:8", "confirmed"],
             "phone" => ["required", "numeric", "digits_between:7,15"],
             "remember" => ["required", "boolean"],
         ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            "username" => trim($this->username),
+        ]);
     }
 }
